a51622e9468caf0d02be9f2955dd2f951e7bf67a
At0micSupra <43355748+At0micSupra@users.noreply.github.com>
2023-08-08 11:01:12 -0700

    wc.c

---
+++ wc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	char c;
+	int l, w, ch;
+	l = w = ch = 0;
+	int word = 0;
+	while((c = getchar()) != EOF) {
+		ch++;
+		if(c == '\n') l++;
+		if(c == ' ' || c == '\n' || c == '\t') {
+			if(word) {
+				w++;
+				word = 0;
+			}
+		} else {
+			word = 1;
+		}
+	}
+	printf("lines: %d, words: %d, chars: %d\n", l, w, ch);
+}
